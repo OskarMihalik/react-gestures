0d450f32fedc3a2b1982015c0c0fbbfe0e969ec9
OskarMihalik <45460241+OskarMihalik@users.noreply.github.com>
2022-05-17 21:10:14 +0200

    feat(): restart button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React, { useRef } from 'react';
 import './App.css';
-import { Canvas } from 'react-three-fiber';
+import { Canvas, MeshProps } from 'react-three-fiber';
 import Mesh from './Mesh';
 
 function App() {
   const componentRef = useRef(null);
+  const meshRef = useRef<MeshProps>()
 
+  const restartPosition = () => {
+    if (!meshRef.current) {
+      return;
+    }
+    meshRef.current.position.x = 0
+    meshRef.current.position.y = 0
+    meshRef.current.position.z = 0
+  }
 
   return (
     <div style={{
@@ -16,8 +25,17 @@ function App() {
         <directionalLight intensity={0.5} />
         <ambientLight intensity={0.5} />
         <spotLight position={[10, 15, 10]} angle={0.9} />
-        <Mesh componentRef={componentRef} />
+        <Mesh componentRef={componentRef} meshRef={meshRef} />
       </Canvas>
+      <button style={{
+        position: 'fixed',
+        left: '50%',
+        transform: 'translateX(-50%)',
+        top: '90%'
+      }}
+        onClick={restartPosition}>
+        restart
+      </button>
     </div>
   );
 }
--- src/Mesh.tsx
+++ src/Mesh.tsx
@@ -1,58 +1,57 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { MeshProps } from 'react-three-fiber'
 import { Vector } from 'ts-matrix'
 import { IPointer, useGestures } from './lib/gestures'
 
 type Props = {
-    componentRef: any
+    componentRef: any,
+    meshRef: React.MutableRefObject<MeshProps | undefined>,
 }
 
 const Mesh = (props: Props) => {
-    const myMeshRef = useRef<MeshProps>()
-
     const onTap = (pointer: IPointer) => {
         console.log("tap:", pointer.position)
     }
 
     const onDrag = (pointer: IPointer) => {
-        if (!myMeshRef.current) {
+        if (!props.meshRef.current) {
             return;
         }
-        myMeshRef.current.position.x = myMeshRef.current.position.x + (pointer.delta.at(0) * 0.05)
-        myMeshRef.current.position.y = myMeshRef.current.position.y - pointer.delta.at(1) * 0.05
+        props.meshRef.current.position.x = props.meshRef.current.position.x + (pointer.delta.at(0) * 0.05)
+        props.meshRef.current.position.y = props.meshRef.current.position.y - pointer.delta.at(1) * 0.05
     }
 
     const onRotate = (rotation: number) => {
-        if (!myMeshRef.current) {
+        if (!props.meshRef.current) {
             return;
         }
-        myMeshRef.current.rotation.y += rotation * 0.1
+        props.meshRef.current.rotation.y += rotation * 0.1
     }
 
     const onPinch = (scale: number) => {
-        if (!myMeshRef.current) {
+        if (!props.meshRef.current) {
             return;
         }
         const pinch = scale * 0.005
-        myMeshRef.current.scale.x += pinch
-        myMeshRef.current.scale.y += pinch
-        myMeshRef.current.scale.z += pinch
+        props.meshRef.current.scale.x += pinch
+        props.meshRef.current.scale.y += pinch
+        props.meshRef.current.scale.z += pinch
     }
 
     const onDoubleDrag = (direction: Vector, pointers: IPointer[]) => {
-        if (!myMeshRef.current) {
+        if (!props.meshRef.current) {
             return;
         }
-        myMeshRef.current.position.x += (direction.at(0) * 0.05)
-        myMeshRef.current.position.z += (direction.at(1) * 0.05)
+        props.meshRef.current.position.x += (direction.at(0) * 0.05)
+        props.meshRef.current.position.z += (direction.at(1) * 0.05)
     }
 
     const onTripleDrag = (direction: Vector, pointers: IPointer[]) => {
-        if (!myMeshRef.current) {
+        if (!props.meshRef.current) {
             return;
         }
 
-        myMeshRef.current.position.y -= (direction.at(1) * 0.05)
+        props.meshRef.current.position.y -= (direction.at(1) * 0.05)
     }
 
     const onHold = (pointer: IPointer) => {
@@ -73,7 +72,7 @@ const Mesh = (props: Props) => {
     )
 
     return (
-        <mesh rotation={[0, 0, 0]} ref={myMeshRef}>
+        <mesh rotation={[0, 0, 0]} ref={props.meshRef}>
             <boxGeometry attach="geometry" args={[2, 2, 2]} />
             <meshStandardMaterial
                 attach="material"
