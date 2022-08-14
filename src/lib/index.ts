db3547973345278df9d332859a297ad8ce23acbc
OskarMihalik <45460241+OskarMihalik@users.noreply.github.com>
2022-08-14 15:59:58 +0200

    feat: removed unnecesarry type export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,4 @@
 import { useGestures } from "./gestures";
-import { IPointer } from "./gestures";
 import useTap from "./useTap";
 
-export { useGestures, IPointer, useTap };
+export { useGestures, useTap };
